2f627212ff5d11bcfdcfcbe7488d321b3d8fbbbc
Eduardo Ramos <eduardoramos@MacBook-Pro-de-Eduardo.local>
2015-03-25 18:03:00 -0300

    Quase pronta

--- FacilitiesViewController.m
+++ FacilitiesViewController.m
@@ -20,46 +20,84 @@
 @property (weak, nonatomic) IBOutlet UIButton *bottom9;
 @property (weak, nonatomic) IBOutlet UIButton *bottom6;
 @property (weak, nonatomic) IBOutlet UIButton *bottom3;
-@property NSArray *numero;
 
+
+@property (weak, nonatomic) IBOutlet UIButton *bottomMais;
+
+@property (weak, nonatomic) IBOutlet UIButton *bottomMenos;
+
+@property (weak, nonatomic) IBOutlet UIButton *bottomPorcento;
+
+@property (weak, nonatomic) IBOutlet UIButton *bottomVezes;
+@property (weak, nonatomic) IBOutlet UIButton *bottomDividir;
+
+@property (weak, nonatomic) IBOutlet UIButton *botttomIgual;
+@property (weak, nonatomic) IBOutlet UIButton *bottomCE;
+@property (weak, nonatomic) IBOutlet UIButton *bottomPoint;
+@property float k,j;
+@property BOOL flag;
+typedef enum{
+    soma,
+    dividir,
+    multiplicar,
+    subtrair
+    
+    
+}operacao;
+@property int op;
 @end
 
 @implementation FacilitiesViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    //self.botton7.layer.cornerRadius = 7.0;
-    self.bottom0.layer.borderWidth = 3.0;
-    self.bottom0.layer.borderColor = [[UIColor whiteColor] CGColor];
-    self.bottom7.layer.borderWidth = 3.0;
-    self.bottom7.layer.borderColor = [[UIColor whiteColor] CGColor];
-    self.bottom4.layer.borderColor = [[UIColor whiteColor] CGColor];
-    self.bottom4.layer.borderWidth = 3.0;
-    self.bottom1.layer.borderColor = [[UIColor whiteColor] CGColor];
-    self.bottom1.layer.borderWidth = 3.0;
-    self.bottom8.layer.borderColor = [[UIColor whiteColor] CGColor];
-    self.bottom8.layer.borderWidth = 3.0;
-    self.bottom5.layer.borderColor = [[UIColor whiteColor] CGColor];
-    self.bottom5.layer.borderWidth = 3.0;
-    self.bottom2.layer.borderColor = [[UIColor whiteColor] CGColor];
-    self.bottom2.layer.borderWidth = 3.0;
-    self.bottom9.layer.borderColor = [[UIColor whiteColor] CGColor];
-    self.bottom9.layer.borderWidth = 3.0;
-    self.bottom6.layer.borderColor = [[UIColor whiteColor] CGColor];
-    self.bottom6.layer.borderWidth = 3.0;
-    self.bottom3.layer.borderColor = [[UIColor whiteColor] CGColor];
-    self.bottom3.layer.borderWidth = 3.0;
-    [self setNumero:[NSArray arrayWithObjects:self.bottom1,self.bottom2,self.bottom3,self.bottom4,self.bottom5,self.bottom6,self.bottom7,self.bottom8,self.bottom9,self.bottom0, nil]];
-    for (int x =0; x<self.numero.count; x++) {
-        UIButton * btn = [[self numero] objectAtIndex:x];
+    NSMutableArray *array = [NSMutableArray arrayWithObjects:self.bottom1,self.bottom2,self.bottom3,self.bottom4,self.bottom5,self.bottom6,self.bottom7,self.bottom8,self.bottom9,self.bottom0,self.bottomPoint, nil];
+    for (int x =0; x<array.count; x++) {
+        UIButton * btn = [array objectAtIndex:x];
+        btn.layer.borderColor = [[UIColor whiteColor] CGColor];
+        btn.layer.borderWidth = 3.0;
+        [btn addTarget:self action:@selector(cliqueBotao:) forControlEvents:UIControlEventTouchUpInside];
+
+    }
+    array = [NSMutableArray arrayWithObjects:self.bottomPorcento,self.bottomVezes,self.botttomIgual, self.bottomCE,self.bottomDividir,self.bottomMais,self.bottomMenos,nil];
+    for (int x =0; x<array.count; x++) {
+        UIButton * btn = [array objectAtIndex:x];
+        btn.layer.borderColor = [[UIColor blackColor] CGColor];
+        btn.layer.borderWidth = 5.0;
         [btn addTarget:self action:@selector(cliqueBotao:) forControlEvents:UIControlEventTouchUpInside];
     }
 }
+//-(void)transformers: (float ) x {
+  //  self.visorLabel.text = float stringWithFormat:@"%f",x
+
+
 -(void)cliqueBotao: (UIButton * )sender{
    // NSLog(@"%@",sender.titleLabel.text);
+    
+    if (sender == self.bottomDividir) {
+        _op = dividir;
+        _j =[[[self visorLabel] text] floatValue];
+         self.visorLabel.text = @"";
+        
+        return;
+        
+        
+    }
+    if (sender == self.botttomIgual) {
+      _k = [[[self visorLabel] text] floatValue];
+        if (_op == dividir) {
+            self.visorLabel.text = [NSString stringWithFormat:@"%.3f",_j/_k];
+            return;
+        }
+    }
+
+    if (sender == self.bottomCE) {
+        self.visorLabel.text =@"";
+        return;
+    }
     self.visorLabel.text = [NSString stringWithFormat:@"%@%@",self.visorLabel.text,sender.titleLabel.text];//sender.titleLabel.text;
     
-
+  
 }
 
 - (void)didReceiveMemoryWarning {
