65a84fb75c74253b405ce1b5fe46922617aaae85
Eduardo Ramos <eduardoramos@MacBook-Pro-de-Eduardo.local>
2015-03-25 13:19:08 -0300

    Initial Commit

---
+++ Facilities/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Facilities
+//
+//  Created by Eduardo Ramos on 25/03/15.
+//  Copyright (c) 2015 Eduardo Ramos. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
